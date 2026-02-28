f45547531d107c48b7374c240e0b39ef527ce282
sidram403 <siddharthpatil9108@gmail.com>
2026-02-28 15:02:18 +0530

    chore: frontend setup with vite and tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-center text-black font-bold">
+      AI Powered Invoice Generator
+    </div>
+  );
+};
+
+export default App;
